dad6deef3c5f681c45bbafdddcb05dce50977a99
Dave Risney <dave@deletethis.net>
2018-05-28 13:32:08 -0700

    Add offline event handling, improve comments of error.js

--- JSBrowser/js/error.js
+++ JSBrowser/js/error.js
@@ -1,4 +1,9 @@
-﻿// read uri and webErrorStatus for query
+﻿// Our contract for the error page is to provide information about the error and the failing URI in the
+// query. We load the error page over ms-appx-web and don't provide it special abilities, so its OK that
+// anyone can navigate us to our own error page with incorrect error information. Instead one could directly
+// navigate to the error page, search page, etc.
+
+// Parse the query property of the URI into name value pairs as properties on a JavaScript object.
 const query = document.location.
     search. // ?a%23b=c%23d&x=y
     substr(1). // a%23b=c%23d&x=y
@@ -13,6 +18,8 @@ const query = document.location.
         return prev;
     }, {});
 
+// Create a map from the element IDs to the properties to change on those IDs. Properties can either be
+// 'uri' which sets the href attribute on the element or 'content' which sets the textContent of the element.
 const idToUri = {
     errorUri: { uri: query.uri, content: query.uri },
     webErrorStatus: { content: query.webErrorStatus },
@@ -21,6 +28,7 @@ const idToUri = {
     archiveUri: { uri: "https://web.archive.org/web/*/" + query.uri },
 };
 
+// Now actually apply the idToUri map to elements.
 Object.keys(idToUri).forEach(name => {
     const element = document.getElementById(name);
     if (idToUri[name].uri) {
@@ -30,3 +38,13 @@ Object.keys(idToUri).forEach(name => {
         element.textContent = idToUri[name].content;
     }
 })
+
+// Handle the common case of losing and gaining network access.
+// If we start the error page with no network access, then its very likely we are on the error page because
+// we have no network access. In that case, listen for the online event to tell us when we reconnect.
+if (!window.navigator.onLine) {
+    document.body.addEventListener("online", () => {
+        // When we reconnect, try the failed URI again.
+        document.location.href = query.uri;
+    });
+}
